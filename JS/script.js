c556e62abc14af5744ec9d5bd09ad68f5d08bab1
sakshi <sakshi08062004@gmail.com>
2026-07-06 09:57:04 +0530

    Initial commit - Simple Interest Calculator

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+function calculateSI() {
+    let P = document.getElementById("principal").value;
+    let R = document.getElementById("rate").value;
+    let N = document.getElementById("time").value;
+
+    let SI = (P * R * N) / 100;
+
+    document.getElementById("result").innerText = "Simple Interest: " + SI;
+}
